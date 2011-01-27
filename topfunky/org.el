7ca3c948e4584cfa6caf8bc6564d37b9ea5ad489
Geoffrey Grosenbach <boss@topfunky.com>
2011-01-27 13:23:11 -0800

    Snippet to add current timestamp. For time tracking.

--- topfunky/org.el
+++ topfunky/org.el
@@ -1,5 +1,12 @@
 ;; org-mode
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
+;; Functions
+(defun timestamp-for-org ()
+  "Insert string for the current time formatted like '2011-01-01 Thu 13:54'."
+  (interactive)
+  (insert (format-time-string "<%Y-%m-%d %a %H:%M>")))
+
 ;; Override
 (add-hook 'org-mode-hook
           (lambda()
@@ -8,6 +15,7 @@
             (local-set-key [(control shift right)] 'next-buffer)
             (local-set-key [(meta shift right)] 'ido-switch-buffer)
             (local-set-key [(meta shift left)] 'magit-status)
+            (local-set-key [(control shift t)] 'timestamp-for-org)
             ))
 
 (setq org-agenda-files (file-expand-wildcards "~/bin/org/*.org"))
